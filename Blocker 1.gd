7fed5b180ba937ceae9fb36d21c92f30c65ded5a
kson4 <60405910+kson4@users.noreply.github.com>
2021-11-16 13:06:48 -0800

    Updated enemy movement

--- Blocker 1.gd
+++ Blocker 1.gd
@@ -1,15 +1,36 @@
 extends KinematicBody2D
 
 var shootingcount = rand_range(0,50)
-export var move_speed = 10
+export var move_speed = 3.5
+
+var screenWidth = ProjectSettings.get_setting("display/window/size/width")
+var screenHeight = ProjectSettings.get_setting("display/window/size/height")
+
+var vertical_distance = 50
+var direction = Vector2(move_speed, 0)
+var move = true
 
 func _physics_process(delta):
-	var collision = move_and_collide(Vector2.DOWN * delta * move_speed)
+	#var collision = move_and_collide(Vector2.DOWN * delta * move_speed)
+	if move:
+		position += direction
+		checkForBorderHit()
+		checkForBottomReached()
 	
 func kill():
 	queue_free()
-	
 
+func checkForBorderHit():
+	if position.x < 0 or position.x >= screenWidth:
+		get_tree().call_group("Blockers1","reverseDirection")
+
+func checkForBottomReached():
+	if position.y >= screenHeight:
+		move = false
+
+func reverseDirection():
+	direction = -direction
+	position.y += vertical_distance
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
