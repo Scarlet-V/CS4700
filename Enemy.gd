603c40d3995c8180a11a42cb9c9bf38a2c0f6c60
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-11-16 10:06:09 -0800

    made enemies move from down to right instead and added wall collision

---
+++ Enemy.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var shootingcount = rand_range(0,50)
+var move_speed = 10
+
+func _physics_process(delta):
+	var collision = move_and_collide(Vector2.RIGHT * delta * move_speed)
+	shootingcount +=delta
+	if shootingcount >=50:
+		var bullet = preload("res://Enemybullet.tscn")
+		var firedbullet = bullet.instance()
+		firedbullet.position = Vector2(position.x,position.y)
+		get_parent().call_deferred("add_child", firedbullet)
+		shootingcount = rand_range(0,50)
+	
+func kill():
+	queue_free()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
