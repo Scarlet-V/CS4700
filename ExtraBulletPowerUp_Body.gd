d5a923c4e7e6bbacd32d5a52beb46f74f2c7869d
kson4 <60405910+kson4@users.noreply.github.com>
2021-11-18 11:31:08 -0800

    update

--- ExtraBulletPowerUp_Body.gd
+++ ExtraBulletPowerUp_Body.gd
@@ -1,12 +1,11 @@
-extends KinematicBody2D
+extends Area2D
 
 var move_speed = 300
 
 func _physics_process(delta):
 	position.y += move_speed * delta
 
-func _on_shot_body_entered(body):
+func _on_ExtraBulletPowerUp_Body_body_entered(body):
 	if body.name == "Player":
-		get_tree().reload_current_scene()
-	if body.name == "Shot":
+		Global.maxbullet += 1
 		queue_free()
--- Global.gd
+++ Global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var bulletAvailable = true
+var maxBullet = 1
 var playerHealth = 1
 var playerMaxHealth = 1
 var playerScore = 0
