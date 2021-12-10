2f5cd5153210b98ae5d976ae80ee860e02efa41f
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-12-09 23:13:58 -0800

    changed clonepu to clone to spawn

--- ClonePU.gd
+++ ClonePU.gd
@@ -5,14 +5,8 @@ var delay = 1;
 onready var cooldownTimer := $CooldownTimer
 
 func _physics_process(delta):
-	position.y += move_speed * delta 
-	if Global.clone == true:
-		if Input.is_action_just_pressed("ui_accept") && Global.clonebulletAvailable == true:
-			print("clone fired")
-			fire()
-			
-	
-	
+		position.y += move_speed * delta 
+
 	
 func _on_ClonePU_body_entered(body):
 	if body.name == "Player":
@@ -20,23 +14,6 @@ func _on_ClonePU_body_entered(body):
 		Global.clone = true
 
 
-func fire():
-	if get_tree().get_current_scene().get_name() == "Variant":
-		var clonebullet = load("res://CloneBullet.tscn")
-		var clonefiredbullet = clonebullet.instance()
-		clonefiredbullet.position = Vector2(position.x, position.y)
-		get_parent().call_deferred("add_child", clonefiredbullet)
-		Global.clonebulletAvailable = false
-	else:
-		if Global.clonecurrentBullet == 0:
-			Global.clonebulletAvailable = false
-			reload()
-
-func reload():
-	print("RELOADING")
-	yield(get_tree().create_timer(1),"timeout")
-	Global.clonecurrentBullet = Global.cloneMaxBullet
-	Global.clonebulletAvailable = true
 
 
 	
